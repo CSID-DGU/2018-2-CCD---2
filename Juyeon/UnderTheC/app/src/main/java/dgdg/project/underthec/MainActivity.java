55a919bb372adb087166b9eee31f0e230f49b64b
박주연 <juyeon0625@users.noreply.github.com>
2018-11-19 14:22:40 +0900

    xml 파싱 (오류)

--- Juyeon/UnderTheC/app/src/main/java/dgdg/project/underthec/MainActivity.java
+++ Juyeon/UnderTheC/app/src/main/java/dgdg/project/underthec/MainActivity.java
@@ -1,62 +1,135 @@
 package dgdg.project.underthec;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends Activity {
 
-    Button btn;
-    TextView result;
+    EditText edit;
+    TextView text;
 
-    //JSON 형식의 String 상수.
-    final String json = "[{\"id\":\"키즈베어\",\"tel\":\"010-1111-2222\"}," +
-            "{\"id\":\"김꽃드래\",\"tel\":\"010-3333-4444\"}," +
-            "{\"id\":\"민식이냐\",\"tel\":\"010-5555-6666\"}]" ;
+    String key="KFwctWcx7gYRywGCv92JmuHsbXXd4o91IkygPefWAMpTbIxYKYEIsuJmOZgAMMncgNBZ40YvN2lw5rtemPnO%2Bg%3D%3D";
+
+    String data;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button)findViewById(R.id.btn);
-        result = (TextView)findViewById(R.id.result);
-
-        btn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                jsonParser();
-            }
-        });
+        edit= (EditText)findViewById(R.id.edit);
+        text= (TextView)findViewById(R.id.text);
     }
 
-    private void jsonParser(){
+    //Button을 클릭했을 때 자동으로 호출되는 callback method
+    public void mOnClick(View v){
 
-        String resultStr = "";
+        switch( v.getId() ){
+            case R.id.button:
+
+                // 급하게 짜느라 소스가 지저분해요..
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+
+                    }
+                }).start();
+
+                break;
+        }
+
+    }
+
+
+    String getXmlData(){
+
+        StringBuffer buffer=new StringBuffer();
+
+        String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
+        String location = URLEncoder.encode(str);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding     //지역 검색 위한 변수
+
+        String queryUrl="http://apis.data.go.kr/1611000/ArchPmsService/getApPklotInfo?"//요청 URL
+                +"sigunguCd="+location
+                +"&bjdongCd=10300&bun=0012&ji=0004&ServiceKey=" + key;
 
         try {
-            //JSON String으로 부터 JSONArray 생성. [](대괄호)
-            JSONArray jArr = new JSONArray(json);
+            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+            InputStream is= url.openStream(); //url위치로 입력스트림 연결
 
-            for (int i = 0; i < jArr.length(); i++) {
-                //JSONArray에서 i번째 해당하는 JSONObject를 추출.
-                JSONObject jObj = jArr.getJSONObject(i);
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
 
-                //각 이름("id"/"tel")에 해당하는 값을 추출.
-                resultStr += String.format("아이디 : %s   전화번호 : %s\n",
-                        jObj.getString("id"), jObj.getString("tel"));
+            String tag;
+
+            xpp.next();
+            int eventType= xpp.getEventType();
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag= xpp.getName();//태그 이름 얻어오기
+
+                        if(tag.equals("item")) ;// 첫번째 검색결과
+                        else if(tag.equals("platPIc")){
+                            buffer.append("주소 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//platPIc 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName(); //태그 이름 얻어오기
+
+                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+
+                        break;
+                }
+
+                eventType= xpp.next();
             }
-            result.setText(resultStr);
-        } catch (JSONException e) {
-            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
         }
+
+        buffer.append("파싱 끝\n");
+
+        return buffer.toString();//StringBuffer 문자열 객체 반환
+
     }
+
 }
