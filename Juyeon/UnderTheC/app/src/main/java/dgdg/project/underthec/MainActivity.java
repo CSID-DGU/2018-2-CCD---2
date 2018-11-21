4c4a45a967fec0b0c50be0ad844a5784869c3ddc
박주연 <juyeon0625@users.noreply.github.com>
2018-11-21 16:01:01 +0900

    CCTV xml 파일 파싱

--- Juyeon/UnderTheC/app/src/main/java/dgdg/project/underthec/MainActivity.java
+++ Juyeon/UnderTheC/app/src/main/java/dgdg/project/underthec/MainActivity.java
@@ -1,135 +1,65 @@
 package dgdg.project.underthec;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Xml;
 import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.io.StringReader;
 
 public class MainActivity extends Activity {
-
-    EditText edit;
-    TextView text;
-
-    String key="KFwctWcx7gYRywGCv92JmuHsbXXd4o91IkygPefWAMpTbIxYKYEIsuJmOZgAMMncgNBZ40YvN2lw5rtemPnO%2Bg%3D%3D";
-
-    String data;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        edit= (EditText)findViewById(R.id.edit);
-        text= (TextView)findViewById(R.id.text);
-    }
-
-    //Button을 클릭했을 때 자동으로 호출되는 callback method
-    public void mOnClick(View v){
-
-        switch( v.getId() ){
-            case R.id.button:
-
-                // 급하게 짜느라 소스가 지저분해요..
-                new Thread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        // TODO Auto-generated method stub
-                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
-
-
-                        runOnUiThread(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                // TODO Auto-generated method stub
-                                text.setText(data); //TextView에 문자열  data 출력
-                            }
-                        });
-
-                    }
-                }).start();
-
-                break;
-        }
-
-    }
-
-
-    String getXmlData(){
-
-        StringBuffer buffer=new StringBuffer();
-
-        String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
-        String location = URLEncoder.encode(str);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding     //지역 검색 위한 변수
-
-        String queryUrl="http://apis.data.go.kr/1611000/ArchPmsService/getApPklotInfo?"//요청 URL
-                +"sigunguCd="+location
-                +"&bjdongCd=10300&bun=0012&ji=0004&ServiceKey=" + key;
-
+        TextView tv=(TextView)findViewById(R.id.data);
+        String file="서울특별시_중구_CCTV_20181101.xml";
+        String result="";
         try {
-            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
-            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+            InputStream is=getAssets().open(file);
+            int size=is.available();
+            byte[] buffer=new byte[size];
+            is.read(buffer);
+            is.close();
+            result=new String(buffer,"utf-8");
 
-            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
-            XmlPullParser xpp= factory.newPullParser();
-            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
-
-            String tag;
-
-            xpp.next();
-            int eventType= xpp.getEventType();
-
-            while( eventType != XmlPullParser.END_DOCUMENT ){
-                switch( eventType ){
-                    case XmlPullParser.START_DOCUMENT:
-                        buffer.append("파싱 시작...\n\n");
-                        break;
-
-                    case XmlPullParser.START_TAG:
-                        tag= xpp.getName();//태그 이름 얻어오기
-
-                        if(tag.equals("item")) ;// 첫번째 검색결과
-                        else if(tag.equals("platPIc")){
-                            buffer.append("주소 : ");
-                            xpp.next();
-                            buffer.append(xpp.getText());//platPIc 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        }
-
-                        break;
-
-                    case XmlPullParser.TEXT:
-                        break;
-
-                    case XmlPullParser.END_TAG:
-                        tag= xpp.getName(); //태그 이름 얻어오기
-
-                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
-
-                        break;
-                }
-
-                eventType= xpp.next();
+            XmlPullParserFactory factory=XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true); //xml 네임스페이스 지원 여부 설정
+            XmlPullParser xpp=factory.newPullParser();
+            xpp.setInput(new StringReader(result));
+            int eventType=xpp.getEventType();
+            /*
+             * eventType
+             * 1.START_TAG
+             * 2.END_TAG
+             * 3.TEXT
+             * etc
+             * */
+            boolean bSet=false;
+            while(eventType!=XmlPullParser.END_DOCUMENT){
+                if(eventType== XmlPullParser.START_TAG){
+                    String tag_name=xpp.getName();
+                    if(tag_name.equals("소재지도로명주소")|tag_name.equals("소재지지번주소")|tag_name.equals("위도")|tag_name.equals("경도"))
+                        bSet=true;
+                }else if(eventType==XmlPullParser.TEXT){
+                    if(bSet){
+                        String data=xpp.getText();
+                        tv.append(data+"\n");
+                        bSet=false;
+                    }
+                }else if(eventType==XmlPullParser.END_TAG);
+                eventType=xpp.next();
             }
 
         } catch (Exception e) {
-            // TODO Auto-generated catch blocke.printStackTrace();
+            e.printStackTrace();
         }
 
-        buffer.append("파싱 끝\n");
-
-        return buffer.toString();//StringBuffer 문자열 객체 반환
-
     }
-
 }
